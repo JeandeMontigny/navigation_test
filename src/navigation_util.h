b8f0fe0b3b4707e4e49c61ae53a2e781f9576078
JeandeMontigny <j.de-montigny2@newcastle.ac.uk>
2020-08-13 16:53:02 +0200

    add bdm to map location conversion and add map_pixel_size parameter for map resolution

--- src/navigation_util.h
+++ src/navigation_util.h
@@ -13,15 +13,43 @@
 namespace bdm {
 
 // ---------------------------------------------------------------------------
+inline double GetBDMToMapLoc(double bdm_loc) {
+  auto* sim = Simulation::GetActive();
+  auto* param = sim->GetParam();
+  auto* sparam = param->GetModuleParam<SimParam>();
+
+  return (bdm_loc + param->max_bound_)/sparam->map_pixel_size;
+}
+// ---------------------------------------------------------------------------
+inline double GetMapToBDMLoc(double map_loc) {
+  auto* sim = Simulation::GetActive();
+  auto* param = sim->GetParam();
+  auto* sparam = param->GetModuleParam<SimParam>();
+
+  return (map_loc * sparam->map_pixel_size) - param->max_bound_;
+}
+
+// ---------------------------------------------------------------------------
+inline double GetMapSize() {
+  auto* sim = Simulation::GetActive();
+  auto* param = sim->GetParam();
+  auto* sparam = param->GetModuleParam<SimParam>();
+
+  return (param->max_bound_*2)/sparam->map_pixel_size;
+}
+
+// ---------------------------------------------------------------------------
   inline std::vector<std::vector<bool>> GetNavigationMap() {
     auto* sim = Simulation::GetActive();
     auto* param = sim->GetParam();
+    auto* sparam = param->GetModuleParam<SimParam>();
 
-    std::vector<std::vector<bool>> navigation_map(param->max_bound_ *2 -1, std::vector<bool>(param->max_bound_*2 -1, true));
+    double map_size = GetMapSize();
+    std::vector<std::vector<bool>> navigation_map(map_size, std::vector<bool>(map_size, true));
 
-    for (int x = 0 ; x < param->max_bound_ *2 -1 ; x++) {
-      for (int y = 0; y < param->max_bound_ *2 -1 ; y++) {
-        Double3 position = {x +0.5 +param->min_bound_, y +0.5 +param->min_bound_, 0.0};
+    for (int x = 0 ; x < map_size ; x += sparam->map_pixel_size) {
+      for (int y = 0; y < map_size ; y += sparam->map_pixel_size) {
+        Double3 position = {x + param->min_bound_, y + param->min_bound_, 0.0};
         if (IsInsideStructure(position)) {
           navigation_map[x][y] = false;
         }
@@ -40,7 +68,10 @@ inline std::vector<Pair> AddDestinationToList(std::vector<Pair> destinations_lis
   //      going to a seat? is destination a wall? etc.
   //      destination also depending on human previously created (same seat?)
   // list of list to check if destination is already taken?
-  destinations_list.push_back(make_pair(900, 50));
+  // destinations_list.push_back(make_pair(GetBDMToMapLoc(450), GetBDMToMapLoc(-450)));
+
+  //TODO: remove hard coded destination
+  destinations_list.push_back(make_pair(GetBDMToMapLoc(140), GetBDMToMapLoc(90)));
 
   return destinations_list;
 } // end AddDestinationToList
