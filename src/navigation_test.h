9d88212717815bec21be411cd6a026d33e4d797a
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2020-10-05 10:47:34 +0200

    remove temporary retun in simulate

--- src/navigation_test.h
+++ src/navigation_test.h
@@ -42,8 +42,6 @@ inline int Simulate(int argc, const char** argv) {
   // export ROOT geometry to OpenFOAM format
   ExportGeomToFoam();
 
-  return 0;
-
   // construct the 2d array for navigation
   std::vector<std::vector<bool>> navigation_map;
 
