401fdf81efc0c7c91a9fecfb7e1eb031813ad3a4
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2020-11-03 21:21:24 +0100

    Improve openlb installation script

--- openlb/install_openlb.sh
+++ openlb/install_openlb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+BDM_SCRIPT_DIR=$(readlink -e $(dirname "${BASH_SOURCE[0]}"))
+cd $BDM_SCRIPT_DIR
+
 git clone https://github.com/openLB/openLB
 cd openLB
 
 git apply ../add_fPIC_flag.patch
 
-make
+make -j$(grep -c ^processor /proc/cpuinfo)
